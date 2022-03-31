92be6c71619b657c6d2017cd7a2d211b9987524c
Partha_wdjy <101883849+partha-wdjy@users.noreply.github.com>
2022-03-31 13:49:59 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -8,8 +8,8 @@ global.anti_delete = false
 
 // setting
 global.ownername ="ZeeoneOfc"
-global.ownernumber = "62887435047326"
-global.botname = "Haruka-Bot"
+global.ownernumber = "6285156521251"
+global.botname = "KRiZ-Bot"
 global.thumbnail = fs.readFileSync("./settings/Haruka.jpg") //sesuaikan dengan nama foto
 global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
 global.lolkey = 'e966d3aac613b61dabd35cc6' //register lolhuman.xyz klo mau apikey
